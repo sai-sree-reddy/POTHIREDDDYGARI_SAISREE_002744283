34321e88b4b7f990ce51db360f2e5c0d92a44731
sai-sree-reddy <pothireddygari.s@northeastern.edu>
2022-10-01 17:26:09 -0400

    Create and View components

---
+++ src/model/Person.java
@@ -0,0 +1,107 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Sonureddy
+ */
+public class Person {
+ 
+    private String name;
+    private String employeeid;
+    private String age;
+    private String gender;
+    private String Startdate;
+    private String level;
+    private String teaminfo;
+    private String positiontitle;
+    private String cellphonenumber;
+    private String emailaddress;
+    //image -->Datatype
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartdate() {
+        return Startdate;
+    }
+
+    public void setStartdate(String Startdate) {
+        this.Startdate = Startdate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeaminfo() {
+        return teaminfo;
+    }
+
+    public void setTeaminfo(String teaminfo) {
+        this.teaminfo = teaminfo;
+    }
+
+    public String getPositiontitle() {
+        return positiontitle;
+    }
+
+    public void setPositiontitle(String positiontitle) {
+        this.positiontitle = positiontitle;
+    }
+
+    public String getCellphonenumber() {
+        return cellphonenumber;
+    }
+
+    public void setCellphonenumber(String cellphonenumber) {
+        this.cellphonenumber = cellphonenumber;
+    }
+
+    public String getEmailaddress() {
+        return emailaddress;
+    }
+
+    public void setEmailaddress(String emailaddress) {
+        this.emailaddress = emailaddress;
+    }
+    
+    //image setter getter 
+    
+}
