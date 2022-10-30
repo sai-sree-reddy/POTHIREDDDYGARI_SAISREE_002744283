b9dfde88f3fe617ed754f149dc783c83906a1148
sai-sree-reddy <pothireddygari.s@northeastern.edu>
2022-10-30 19:22:36 -0400

    "Modified classes"

--- Assignment-2/JavaApplication10/src/Model/Diagnosis.java
+++ Assignment-2/JavaApplication10/src/Model/Diagnosis.java
@@ -1,54 +1,38 @@
+package Model;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
 
 /**
  *
  * @author Sonur
  */
 public class Diagnosis {
-
-    public int getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(int patientId) {
-        this.patientId = patientId;
-    }
-
-    public String getPatientName() {
-        return patientName;
-    }
-
-    public void setPatientName(String patientName) {
-        this.patientName = patientName;
-    }
-
-   
     public int patientId;
     public String patientName;
-    public String age;
-    public String vitalsign;
-    public String pulserate;
-    public String temperature;
-    public String weight;
-    public String sugar;
-    public int doctorid;
-   
+    public String patientEmail;
+    public int reportId;
+    public String reportTitle;
+    public String reportDescription;
+    public String reportVitalSign;
 
-    public Diagnosis(int patientId, String patientName, String age, int doctorid, String vitalsign, String pulserate, String temperature) {
+    public Diagnosis(int patientId,
+            String patientName,
+            String patientEmail,
+            int reportId,
+            String reportTitle,
+            String reportDescription,
+            String reportVitalSign) {
         
         this.patientId = patientId;
         this.patientName = patientName;
-        this.age=age;
-        this.vitalsign=vitalsign;
-        this.pulserate=pulserate;
-        this.temperature=temperature;
-        this.weight=weight;
-        this.sugar=sugar;
-        this.doctorid=doctorid;
+        this.patientEmail = patientEmail;
+        this.reportId = reportId;
+        this.reportTitle = reportTitle;
+        this.reportDescription = reportDescription;
+        this.reportVitalSign = reportVitalSign;
         
     }
 }
